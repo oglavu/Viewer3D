a60c3346f39624e3c097e229c856dad03ec321df
Ognjen <ognjenmirkovic032@gmail.com>
2024-11-20 00:25:35 +0100

    shader uniforms

--- src/Shader.hpp
+++ src/Shader.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <memory>
+#include <glm\glm.hpp>
 
 class Shader {
 private:
@@ -16,6 +17,13 @@ public:
 	Shader(std::string vsFileName, std::string fsFileName);
 
 	void use() const;
+
+	inline void uniform(const std::string, const glm::mat4& ) const;
+	inline void uniform(const std::string, const glm::mat3& ) const;
+	inline void uniform(const std::string, const glm::vec3& ) const;
+	inline void uniform(const std::string, const glm::vec4& ) const;
+	inline void uniform(const std::string, const int) const;
+	inline void uniform(const std::string, const float) const;
 
 };
 
